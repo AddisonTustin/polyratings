f5d9298b327f262fa6dcf9f45c6bc408f2b0100f
Addison Tustin <addisontustin@gmail.com>
2022-03-18 02:46:46 -0700

    Change variable name for s3 uri

--- cloudfront-deploy.sh
+++ cloudfront-deploy.sh
@@ -17,5 +17,5 @@ export AWS_ACCESS_KEY_ID=$ROLE_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$ROLE_SECRET_ACCESS_KEY
 export AWS_SESSION_TOKEN=$ROLE_SESSION_TOKEN
 
-aws s3 sync --delete ./packages/frontend/dist s3://$POLYRATINGS_REPO_URI
+aws s3 sync --delete ./packages/frontend/dist/ s3://$POLYRATINGS_S3_URI
 aws --output text cloudfront create-invalidation --distribution-id $POLYRATINGS_DISTRIBUTION_ID --paths "/*"
