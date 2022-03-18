78a3b48d92f3e83247dfb498c6989f7cc3208f8a
AddisonTustin <addisontustin@gmail.com>
2022-03-18 05:27:43 -0700

    I'm dumb and accidentally uploaded the entire repo to S3

--- cloudfront-deploy.sh
+++ cloudfront-deploy.sh
@@ -3,19 +3,15 @@
 # This file *should* automatically deploy the frontend build artifacts to a specified
 # AWS Cloudfront Distribution
 
+# If we don't set this, the aws cli calls will use 'less' and the script will hang
 export AWS_PAGER=""
 
-CURRENT_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-CURRENT_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-
 aws sts assume-role --role-arn $ROLE_ARN --role-session-name manual-cli-test > sts-output
-ROLE_ACCESS_KEY_ID=$(cat sts-output | sed -n 's/\s*"AccessKeyId": "\([a-zA-Z0-9]*\)",.*/\1/p')
-ROLE_SECRET_ACCESS_KEY=$(cat sts-output | sed -n 's/\s\+"SecretAccessKey": "\(.\+\)",\s*/\1/p')
-ROLE_SESSION_TOKEN=$(cat sts-output | sed -n 's/\s\+"SessionToken": "\(.\+\)",\s*/\1/p')
 
-export AWS_ACCESS_KEY_ID=$ROLE_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$ROLE_SECRET_ACCESS_KEY
-export AWS_SESSION_TOKEN=$ROLE_SESSION_TOKEN
+# Extracts the AWS Credentials from the response
+export AWS_ACCESS_KEY_ID=$ROLE_ACCESS_KEY_ID=$(cat sts-output | sed -n 's/\s*"AccessKeyId": "\([a-zA-Z0-9]*\)",.*/\1/p')
+export AWS_SECRET_ACCESS_KEY=$ROLE_SECRET_ACCESS_KEY=$(cat sts-output | sed -n 's/\s\+"SecretAccessKey": "\(.\+\)",\s*/\1/p')
+export AWS_SESSION_TOKEN=$ROLE_SESSION_TOKEN=$(cat sts-output | sed -n 's/\s\+"SessionToken": "\(.\+\)",\s*/\1/p')
 
-aws s3 sync --delete ./ s3://$POLYRATINGS_S3_URI
+aws s3 sync --delete ./packages/frontend/dist/ s3://$POLYRATINGS_S3_URI
 aws --output text cloudfront create-invalidation --distribution-id $POLYRATINGS_DISTRIBUTION_ID --paths "/*"
